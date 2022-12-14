c80c788e9da4c4f48d79e91737a78203f8ce6cd1
aarotukia <tukiaaro@gmail.com>
2022-12-14 11:20:44 +0200

    Added css and total expenses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header  from './components/Header';
+import TotalExpenses  from './components/TotalExpenses';
 
 function App() {
   return (
     <div>
      <Header />
+     <div className="container">
+       <TotalExpenses />
+       </div>
  <p> hello testing testing</p>
     </div>
   );
---
+++ src/components/TotalExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TotalExpenses = () => {
+  return (
+    <div>
+      <h4> Total Expenses </h4>
+      <h1 id="TotalExpense"> 0.00€</h1> 
+    </div>
+  )
+}
+
+export default TotalExpenses
