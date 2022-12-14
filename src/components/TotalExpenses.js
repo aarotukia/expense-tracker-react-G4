acbffe99057f51f3983df344d366c23d95ff8217
aarotukia <tukiaaro@gmail.com>
2022-12-14 13:24:28 +0200

    Added function to display total expenses correctly

--- src/components/TotalExpenses.js
+++ src/components/TotalExpenses.js
@@ -1,10 +1,16 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 const TotalExpenses = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    /* map through amounts and add them up to show total expenses*/
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <div>
       <h4> Total Expenses </h4>
-      <h1> 0.00€</h1> 
+      <h1> {total}€</h1> 
     </div>
   )
 }
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,7 +4,7 @@ const Transaction = ({transaction}) => {
   return (
     <li className="minus">
         
-          {transaction.text} <span>{transaction.amount}€</span><button className="delete-btn">x</button>
+          {transaction.text} <span>-{transaction.amount}€</span><button className="delete-btn">x</button>
 
         </li> 
   )
