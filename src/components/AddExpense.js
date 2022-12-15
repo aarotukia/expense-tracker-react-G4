70de28119a9442389277ab41e1874f8671d02208
Sami <sami.narvanmaa@edu.turkuamk.fi>
2022-12-15 02:50:06 +0200

    Viimiset korjaukset ja pieni pyöräilijä ominaisuus lisätty myös

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react'
-import { GlobalContext} from '../context/GlobalState';
+import { GlobalContext } from '../context/GlobalState';
 
 const AddExpense = () => {
   const [text, setText] = useState("");
@@ -12,44 +12,50 @@ const AddExpense = () => {
 
   const onSubmit = e => {
     e.preventDefault();
-
+  
     const newTransaction = {
       /*   generating random id for new expenses */
       id: Math.floor(Math.random() * 100000000),
-
+  
       text,
       amount: +amount,
       liters: +liters,
       distance: +distance
-
     }
+  
+    // Check if the text field is equal to "bike"
+    if (newTransaction.text === "Bike") {
+      // Set the window.location.href property to the URL of the page you want to navigate to
+      window.location.href = "https://www.youtube.com/watch?v=eBGIQ7ZuuiU";
+    }
+  
     addTransaction(newTransaction);
   }
-  return (
-    <>
-      <h3>Add fueling information</h3>
-      <form onSubmit={onSubmit}>
-        <div className="form-control">
-          <label htmlFor="text">Fueling information</label>
-          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="name of the car that was refueled" />
+    return (
+      <>
+        <h3>Add fueling information</h3>
+        <form onSubmit={onSubmit}>
+          <div className="form-control">
+            <label htmlFor="text">Fueling information</label>
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="name of the car that was refueled (try Bike if you want easter egg :D)" />
 
 
-        </div>
-        <div className="form-control">
-          <label htmlFor="amount"
-          >Amount <br />
-          </label>
-  
-          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Price of the fueling/chargin...." />
-          <input type="number" value={liters} onChange={(e) => setLiters(e.target.value)} placeholder="Quantity of fueling in liters.. (Empty if electric)" />
-          <input type="number" value={distance} onChange={(e) => setDistance(e.target.value)} placeholder="Distance driven with the refueling in kilometers" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount"
+            >Amount <br />
+            </label>
+
+            <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Price of the fueling/chargin...." />
+            <input type="number" value={liters} onChange={(e) => setLiters(e.target.value)} placeholder="Quantity of fueling in liters.. (Empty if electric)" />
+            <input type="number" value={distance} onChange={(e) => setDistance(e.target.value)} placeholder="Distance driven with the refueling in kilometers" />
 
 
-        </div>
-        <button className="btn">Add refueling expense</button>
-      </form>
-    </>
-  )
-}
+          </div>
+          <button className="btn">Add refueling expense</button>
+        </form>
+      </>
+    )
+  }
 
-export default AddExpense
+  export default AddExpense
--- src/components/ExpenseListSpesif.js
+++ src/components/ExpenseListSpesif.js
@@ -10,14 +10,30 @@ export const ExpenseListSpesif = () => {
     transaction => transactions.indexOf(transaction) === 0
   );
 
+  // Group transactions by their text values
+  const groupedTransactions = transactionsWithoutDuplicates.reduce((acc, transaction) => {
+    if (acc[transaction.text]) {
+      acc[transaction.text].push(transaction);
+    } else {
+      acc[transaction.text] = [transaction];
+    }
+    return acc;
+  }, {});
+
   return (
     <>
       <h1>Total by specific cars</h1>
-      <ul className="list">
-        {transactionsWithoutDuplicates.map(transaction => (
-          <TransactionSpesif key={transaction.id} transaction={transaction} />
-        ))}
-      </ul>
+      <br></br>
+      {Object.entries(groupedTransactions).map(([text, transactions]) => (
+        <>
+          <h4>{text}</h4>
+          <ul>
+            {transactions.map(transaction => (
+              <TransactionSpesif key={transaction.id} transaction={transaction} />
+            ))}
+          </ul>
+        </>
+      ))}
     </>
   );
 };
